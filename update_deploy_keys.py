f2d98049f70b220f6b11757d00023e56016a275e
Stefan Hartmann <stefan.hartmann@leica-geosystems.com>
2020-09-24 11:00:47 +0200

    started script to update ssh keys programmatically

---
+++ update_deploy_keys.py
@@ -0,0 +1,8 @@
+# TODO:
+# 1) Implement click skeleton
+# 2) Options: -t/--token, -u/--user
+# 3) Read all origin URLs from .github/workflows/main.yml
+# 4) Foreach URL:
+# 4a) Generate ssh key pair
+# 4b) Refresh #REPO#_SSH_PRIVATE_KEY in secrets of repo-sync.git
+# 4c) Refresh REPO_SYNC deploy key in fork/mirror.git
