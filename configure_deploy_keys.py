f2bcc1396d73c2ea797d945f73526acaa6ddc1d8
Stefan Hartmann <stefan.hartmann@leica-geosystems.com>
2020-09-24 14:54:42 +0200

    skeleton stuff

--- configure_deploy_keys.py
+++ configure_deploy_keys.py
@@ -8,6 +8,7 @@
 # 4c) Refresh REPO_SYNC deploy key in fork/mirror.git
 from typing import Tuple
 
+import click
 from cryptography.hazmat.backends import default_backend
 from cryptography.hazmat.primitives import serialization
 from cryptography.hazmat.primitives.asymmetric import rsa
@@ -16,14 +17,17 @@ GITHUB_API_URL = 'https://github.com.com/api/v3/'
 GITHUB_ORGANIZATION = 'hexagon-geo-surv'
 
 
-def main():
+@click.command()
+@click.option('-u', '--user', required=True, envvar='GITHUB_USER', help='GitHub.com - User')
+@click.option('-t', '--token', required=True, envvar='GITHUB_TOKEN', help='GitHub.com - Personal Access Token')
+def main(github_user: str, github_token: str) -> None:
     for repo_name in get_repo_names():
         private_key, public_key = __generate_ssh_key_pair()
 
-        set_secret(f'{repo_name.upper()}_SSH_PRIVATE_KEY', private_key)
-        set_secret(f'{repo_name.upper()}_SSH_PUBLIC_KEY', public_key)
+        set_secret(f'{repo_name.upper()}_SSH_PRIVATE_KEY', private_key, github_user, github_token)
+        set_secret(f'{repo_name.upper()}_SSH_PUBLIC_KEY', public_key, github_user, github_token)
 
-        set_deploy_key(repo_name, "REPO_SYNC", public_key)
+        set_deploy_key(repo_name, "REPO_SYNC", public_key, github_user, github_token)
 
         print(f'Configured "{GITHUB_ORGANIZATION}/{repo_name}.git"')
 
@@ -48,11 +52,14 @@ def __generate_ssh_key_pair() -> Tuple:
     return private_key, public_key
 
 
-def set_secret(name, value):
+def set_secret(secret_name: str, secret_value: str, github_user: str, github_token: str) -> None:
+    # TODO
     pass
 
 
-def set_deploy_key(repo_name, name, public_key):
+def set_deploy_key(repo_name: str, deploy_key_name: str, deploy_key_value: str, github_user: str,
+                   github_token: str) -> None:
+    # TODO
     pass
 
 
